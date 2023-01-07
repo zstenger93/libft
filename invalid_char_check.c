0089a3a4a7dbefc9cf74d40f089e01be6d896516
zstenger <zstenger@student.42heilbronn.de>
2023-01-07 20:07:27 +0100

    fix

--- invalid_char_check.c
+++ invalid_char_check.c
@@ -5,18 +5,18 @@ void	invalid_char_check(char *str)
 	int	i;
 
 	if (ft_strlen(str) > 11)
-		return(1);
+		return ;
 	i = 0;
 	if (str[0] == '\0')
-		return(1);
+		return ;
 	if (str[i] == '-')
 		i++;
 	if (str[0] == '-' && str[1] == '\0')
-		return(1);
+		return ;
 	while (str[i] != '\0')
 	{
 		if (ft_is_digit(str[i]) == false)
-			return(1);
+			return ;
 		i++;
 	}
 }
--- valid_int.c
+++ valid_int.c
@@ -3,5 +3,5 @@
 void	valid_int(long res)
 {
 	if (res < INT_MIN || res > INT_MAX)
-		return(1);
+		return ;
 }
